f79c8ae6a131c406962f388bdd0f50ce454744d6
Elysee CONFIANCE <ccelyse1@gmail.com>
2023-10-10 15:35:40 +0200

    added new message on thank you v3

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ["@fullcalendar/react", "@fullcalendar/daygrid", "@fullcalendar/timegrid", "@fullcalendar/interaction"],
-    },
-  },
+  // build: {
+  //   rollupOptions: {
+  //     external: ["@fullcalendar/react", "@fullcalendar/daygrid", "@fullcalendar/timegrid", "@fullcalendar/interaction"],
+  //   },
+  // },
 })
