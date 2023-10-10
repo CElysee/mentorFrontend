641262c567d4ad3401646eeb9e190acae0890e1a
Elysee CONFIANCE <ccelyse1@gmail.com>
2023-10-10 16:11:38 +0200

    added new message on thank you v4

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 import Layout from "./Layout/Layout";
 
-
 function App() {
   const [count, setCount] = useState(0);
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  }, []);
 
   return (
-    <Layout />
+    <>
+      {loading ? (
+        <div className="loader-container">
+          <div className="spinner"></div>
+        </div>
+      ) : (
+        <Layout />
+      )}
+    </>
   );
 }
 
